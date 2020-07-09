c8ae514e9f1788a4bf766cab6e0c4de408ce0232
SKumar180 <shubhamkumar1805070@gmail.com>
2020-07-09 10:09:37 +0530

    added login and signup router file

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const mongoose=require("mongoose");
 const bodyParser = require('body-parser');
 const path=require("path");
 const config= require("./dev.json");
-const mainRoute=require("./server/routes/index")
-const courseRoute=require("./server/routes/course")
+const mainRoute=require("./server/routes/index");
+const courseRoute=require("./server/routes/course");
+const loginRoute=require("./server/routes/userlogin");
+const signupRoute=require("./server/routes/usersignup");
+const User = require("./server/model/user");
 const app=express();
 var port=process.env.PORT || 8080;
-var User = require('/model/user');
+
 mongoose.set('useCreateIndex', true);
 //Connect to Mongo using mongoose
 /* app.use(function(req, res, next) {
@@ -26,6 +29,9 @@ mongoose.set('useCreateIndex', true);
     }
 }); */
 
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
 //connecting to database
 mongoose.connect(config.connectionstring , { useNewUrlParser: true , useUnifiedTopology: true});
 mongoose.set('useFindAndModify', false);
@@ -40,12 +46,17 @@ app.get('/',(req,res)=>{
     res.status(200).send('Hi welcome to Login and Signup API');
 })
 
-
-
-
+app.use("/signup",signupRoute)
+app.use("/login",loginRoute)
 app.use("/",mainRoute)
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.get("/signup",function(req,res){
+    res.sendFile(__dirname + "/public/" + "signup.html")
+})
+app.get("/login",function(req,res){
+    res.sendFile(__dirname + "/public/" + "login.html")
+})
 app.get("/admin",function(req,res){
     res.sendFile(__dirname + "/public/" + "admin.html")
 })
