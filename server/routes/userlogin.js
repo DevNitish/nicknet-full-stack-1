0aa67b1755ec4765d5f6b9094668b53047aa118a
SKumar180 <shubhamkumar1805070@gmail.com>
2020-07-12 15:26:03 +0530

    new CRUD routes are added and tested

--- server/routes/userlogin.js
+++ server/routes/userlogin.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router =express.Router();
+const path = require('path');
 const User = require("../model/user");
-
 
 
   router.post("/login", async (req, res) => {
     var newUser = {};
     newUser.name = req.body.name;
     newUser.password = req.body.password;
+    newUser.email = req.body.email;
   
-    await User.findOne({ name: newUser.name })
+    await User.findOne({ name: newUser.name, email:newUser.email})
       .then(profile => {
         if (!profile) {
           res.send("User not exist");
@@ -24,6 +25,67 @@ const User = require("../model/user");
       .catch(err => {
         console.log("Error is ", err.message);
       });
+  });
+
+  router.get("/showusers",function(req,res){
+    User.findAllUser(function(err,result){
+      if(err){
+          res.status(400).send('Users not found!');
+      }else{
+          res.send(result);
+          console.log(result);
+      }
+      })
+  });
+
+  router.post('/saveUser1', function(req, res,next) {
+
+    User.saveUser(req.body, function(err, result) {
+      if (result) {
+              console.log("Inserted");
+  
+        res.send(result);
+          
+      } else {
+        console.log("Error");
+         
+        res.status(500).send('Internal error occurred--500');
+      }
+    });
+  });
+   
+  router.post('/editUser1', function(req, res,next) {
+    console.log("here",req.body);
+  
+  User.editUser(req.body, function(err, result) {
+    if (result) {
+            console.log("Edited",result);
+  
+      res.send(result);
+        
+    } else {
+      console.log("Error");
+       
+      res.status(500).send('Internal error occurred--500');
+    }
+  });
+  });
+  
+  router.delete('/deleteUser1', function(req, res,next) {
+    console.log("here deleteUser",req.body);
+  
+  User.deleteUser(req.body._id, function(err, result) {
+    if (result) {
+            console.log("Deleted!");
+  
+      res.send(result);
+        
+    } else {
+      console.log("Error");
+       
+      res.status(500).send('Internal error occurred--500');
+    }
+  });
   });
 
   module.exports = router;
