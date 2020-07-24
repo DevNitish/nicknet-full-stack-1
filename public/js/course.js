63670a9d931cd4f8495b4b40ad45b696d07a0320
Nitish Kumar <nitishonspeed@gmai.com>
2020-07-25 00:26:17 +0530

    updated look and feel

--- public/js/course.js
+++ public/js/course.js
@@ -9,8 +9,9 @@ function getModel(id) {
     })
     $("#exampleModalLabel").html(course.courseName)
     $("#modelDesc").html(course.courseDesc)
-    $("#modelDuration").html(course.courseDuration + " Weeks")
-    $("#modelPrice").html(course.coursePrice == 0 ? "FREE! " : course.coursePrice)
+    $("#modelDuration").html("Duration: "+course.courseDuration + " Weeks")
+    let fee=course.coursePrice == 0 ? "FREE! " : course.coursePrice
+    $("#modelPrice").html("")
     $('#modelImage').attr('src', course.courseBanner);
 
 }
